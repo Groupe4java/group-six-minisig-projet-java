137605baf2fb748768b3a3bbea2c7094589df110
rouzies.gaetan@gmail.com <rouzies.gaetan@gmail.com@24aa1d79-986b-93e6-2049-f641ace5f498>
2014-02-06 22:43:05 +0000

    NEED FOR EDITION

--- TestWindowsBuild/src/test/windowsbuilder/ModeConsultation.java
+++ TestWindowsBuild/src/test/windowsbuilder/ModeConsultation.java
@@ -27,6 +27,9 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JList;
 
 public class ModeConsultation extends JFrame {
 
@@ -49,6 +52,9 @@ public class ModeConsultation extends JFrame {
 	
 	boolean lieuSelected = false;
 	boolean parcoursSelected = false;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 	
 	//Fonction main
 	public static void main(String[] args) {
@@ -84,13 +90,28 @@ public class ModeConsultation extends JFrame {
 		contentPane.add(panelCenter, BorderLayout.CENTER);
 		panelCenter.setLayout(new BorderLayout(0, 0));
 		
+		
+		
+		panelMAP = new MyJPanelMap();
+		
+
+		panelMAP.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panelCenter.add(panelMAP, BorderLayout.CENTER);
+		
+		Component horizontalStrut = Box.createHorizontalStrut(400);
+		panelMAP.add(horizontalStrut);
+		
+		JPanel panelDroite = new JPanel();
+		panelCenter.add(panelDroite, BorderLayout.EAST);
+		panelDroite.setLayout(new BoxLayout(panelDroite, BoxLayout.LINE_AXIS));
+		
 		JPanel panelPOI = new JPanel();
+		panelDroite.add(panelPOI);
 		panelPOI.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panelCenter.add(panelPOI, BorderLayout.EAST);
-		panelPOI.setLayout(new BorderLayout(0, 0));
+		panelPOI.setLayout(new BoxLayout(panelPOI, BoxLayout.PAGE_AXIS));
 		
 		JPanel panelPoiNORTH = new JPanel();
-		panelPOI.add(panelPoiNORTH, BorderLayout.NORTH);
+		panelPOI.add(panelPoiNORTH);
 		panelPoiNORTH.setLayout(new BoxLayout(panelPoiNORTH, BoxLayout.PAGE_AXIS));
 		
 		JPanel panelPusher = new JPanel();
@@ -111,7 +132,7 @@ public class ModeConsultation extends JFrame {
 		panelNavigate.add(btnNext);
 		
 		JPanel panelPoiCENTER = new JPanel();
-		panelPOI.add(panelPoiCENTER, BorderLayout.CENTER);
+		panelPOI.add(panelPoiCENTER);
 		panelPoiCENTER.setLayout(new BoxLayout(panelPoiCENTER, BoxLayout.PAGE_AXIS));
 		
 		JPanel panelTitle = new JPanel();
@@ -134,21 +155,155 @@ public class ModeConsultation extends JFrame {
 		panelPoiCENTER.add(panelImage);
 		
 		JPanel panelPoiSOUTH = new JPanel();
-		panelPOI.add(panelPoiSOUTH, BorderLayout.SOUTH);
+		panelPOI.add(panelPoiSOUTH);
 		
 		Component verticalStrut = Box.createVerticalStrut(20);
 		panelPoiSOUTH.add(verticalStrut);
 		
+		JPanel panelEdition = new JPanel();
+		panelDroite.add(panelEdition);
+		panelEdition.setLayout(new BoxLayout(panelEdition, BoxLayout.PAGE_AXIS));
 		
+		JPanel panelEditTitle = new JPanel();
+		panelEdition.add(panelEditTitle);
 		
-		panelMAP = new MyJPanelMap();
+		JLabel label = new JLabel("EDIT Type");
+		panelEditTitle.add(label);
 		
-
-		panelMAP.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panelCenter.add(panelMAP, BorderLayout.CENTER);
+		JPanel panelEditButtonTop = new JPanel();
+		panelEdition.add(panelEditButtonTop);
 		
-		Component horizontalStrut = Box.createHorizontalStrut(400);
-		panelMAP.add(horizontalStrut);
+		JButton button = new JButton("MODIF");
+		panelEditButtonTop.add(button);
+		
+		JButton button_1 = new JButton("DELETE");
+		panelEditButtonTop.add(button_1);
+		
+		JButton button_2 = new JButton("ADD");
+		panelEditButtonTop.add(button_2);
+		
+		JPanel panelEditPart = new JPanel();
+		panelEdition.add(panelEditPart);
+		panelEditPart.setLayout(new BoxLayout(panelEditPart, BoxLayout.PAGE_AXIS));
+		
+		JPanel panelEditComboBox = new JPanel();
+		panelEditPart.add(panelEditComboBox);
+		panelEditComboBox.setLayout(new BoxLayout(panelEditComboBox, BoxLayout.PAGE_AXIS));
+		
+		JPanel panelEditChoixLieu = new JPanel();
+		panelEditComboBox.add(panelEditChoixLieu);
+		
+		JLabel label_1 = new JLabel("Choix Lieu :");
+		label_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		panelEditChoixLieu.add(label_1);
+		
+		JComboBox comboBox = new JComboBox();
+		panelEditChoixLieu.add(comboBox);
+		
+		JPanel panelEditChoixType = new JPanel();
+		panelEditComboBox.add(panelEditChoixType);
+		
+		JLabel label_2 = new JLabel("Choix [Type] :");
+		label_2.setFont(new Font("Tahoma", Font.BOLD, 12));
+		panelEditChoixType.add(label_2);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		panelEditChoixType.add(comboBox_1);
+		
+		JPanel panelEditTextBox = new JPanel();
+		panelEditPart.add(panelEditTextBox);
+		panelEditTextBox.setLayout(new BoxLayout(panelEditTextBox, BoxLayout.PAGE_AXIS));
+		
+		JPanel panelEditTextBoxLibelle = new JPanel();
+		panelEditTextBox.add(panelEditTextBoxLibelle);
+		
+		JLabel label_3 = new JLabel("Libelle :");
+		label_3.setFont(new Font("Tahoma", Font.BOLD, 12));
+		panelEditTextBoxLibelle.add(label_3);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		panelEditTextBoxLibelle.add(textField);
+		
+		JPanel panelEditTextBoxDescription = new JPanel();
+		panelEditTextBox.add(panelEditTextBoxDescription);
+		
+		JLabel label_4 = new JLabel("Description :");
+		label_4.setFont(new Font("Tahoma", Font.BOLD, 12));
+		panelEditTextBoxDescription.add(label_4);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		panelEditTextBoxDescription.add(textField_1);
+		
+		JPanel panelEditTextBoxImage = new JPanel();
+		panelEditTextBox.add(panelEditTextBoxImage);
+		
+		JLabel label_5 = new JLabel("Image :");
+		label_5.setFont(new Font("Tahoma", Font.BOLD, 12));
+		panelEditTextBoxImage.add(label_5);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		panelEditTextBoxImage.add(textField_2);
+		
+		JPanel panelEditPosition = new JPanel();
+		panelEditPart.add(panelEditPosition);
+		
+		JButton button_3 = new JButton("POSITION");
+		panelEditPosition.add(button_3);
+		
+		JLabel label_6 = new JLabel("X");
+		label_6.setFont(new Font("Tahoma", Font.BOLD, 14));
+		panelEditPosition.add(label_6);
+		
+		JLabel label_7 = new JLabel("-");
+		label_7.setFont(new Font("Tahoma", Font.BOLD, 13));
+		panelEditPosition.add(label_7);
+		
+		JLabel label_8 = new JLabel("Y");
+		label_8.setFont(new Font("Tahoma", Font.BOLD, 14));
+		panelEditPosition.add(label_8);
+		
+		JPanel panelEditChoixPOI = new JPanel();
+		panelEditPart.add(panelEditChoixPOI);
+		
+		JLabel label_9 = new JLabel("Choix POI :");
+		label_9.setFont(new Font("Tahoma", Font.BOLD, 13));
+		panelEditChoixPOI.add(label_9);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		panelEditChoixPOI.add(comboBox_2);
+		
+		JButton button_4 = new JButton("+");
+		button_4.setFont(new Font("Tahoma", Font.BOLD, 13));
+		panelEditChoixPOI.add(button_4);
+		
+		JPanel panelEditListPOI = new JPanel();
+		panelEditPart.add(panelEditListPOI);
+		
+		JList<? extends E> list = new JList();
+		panelEditListPOI.add(list);
+		
+		JButton button_5 = new JButton("REMOVE");
+		panelEditListPOI.add(button_5);
+		
+		JPanel panelEditButtonBot = new JPanel();
+		panelEdition.add(panelEditButtonBot);
+		panelEditButtonBot.setLayout(new BoxLayout(panelEditButtonBot, BoxLayout.PAGE_AXIS));
+		
+		JPanel panelEditButtonValidate = new JPanel();
+		panelEditButtonBot.add(panelEditButtonValidate);
+		
+		JButton button_6 = new JButton("VALIDER");
+		button_6.setFont(new Font("Tahoma", Font.BOLD, 12));
+		panelEditButtonValidate.add(button_6);
+		
+		JPanel panelEditButtonCancel = new JPanel();
+		panelEditButtonBot.add(panelEditButtonCancel);
+		
+		JButton button_7 = new JButton("ANNULER");
+		panelEditButtonCancel.add(button_7);
 
 		JPanel panelNorth = new JPanel();
 		panelNorth.setBorder(new LineBorder(new Color(0, 0, 0)));
